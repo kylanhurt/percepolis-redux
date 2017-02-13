e4c1245d97a72bfab05901242cc71ee632360f02
kylanhurt <kylan@datagoniaweb.com>
2017-02-13 00:36:06 -0800

    Fix redux-logger issue

--- src/index.js
+++ src/index.js
@@ -4,21 +4,28 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';  
 import { Router, browserHistory } from 'react-router';  
 import reduxThunk from 'redux-thunk';  
+import createLogger from 'redux-logger';
 import routes from './routes';  
 import reducers from './reducers/index';  
 import { AUTH_USER } from './actions/types';
 import cookie from 'react-cookie';
 import css from './style.less'
-
 // Import stylesheets like this, if you choose: import './public/stylesheets/base.scss';
 
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);  
+const logger = createLogger();
+const createStoreWithMiddleware = applyMiddleware(reduxThunk, logger)(createStore);  
 const store = createStoreWithMiddleware(reducers);
+
+store.subscribe(() => {
+	console.log('store change (from index.js)', store.getState());
+})
 
 const token = cookie.load('token');
 
 if (token) {  
   store.dispatch({ type: AUTH_USER });
+} else { //remove this clause
+	store.dispatch({type: "UNAUTH_USER", payload: "unauth"}); //payload can be an OBJECT
 }
 
 ReactDOM.render(  
